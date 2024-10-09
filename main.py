27d44d8958168ca021291387cd73f53c7da1925b
Martin Ronning <ronningmartin@outlook.com>
2024-10-09 15:02:32 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+# clip creation API
+# video AI API
+# music AI API
+# text AI API
+# speech AI API
+
+x = "prompt"
+video = video-api-call(x)
+music = audio-api-call("apple commerical")
+text = text-api-call(x)
+speech = speech-api-call(text)
+
+clip = clip-creation-api(video, music, speech)
