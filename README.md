789aa5325d205533a9569d2ecf7820ec64abfe59
Martin Ronning <ronningmartin@outlook.com>
2024-10-09 16:02:33 +0200

    add text prompt through CLI argument

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 
 Steps:
-1. https://ai.google.dev/gemini-api/docs/quickstart?lang=python
+1. Ensure you have python version 3.10 or above BEFORE running any pip installs (recommend pyenv for python version management)
+2. run `pip install --upgrade google-api-python-client`
+3. https://ai.google.dev/gemini-api/docs/quickstart?lang=python
 Follow this to install google-generativeui and to create a key
-2. Once you have the key, run `python integrate_key.py [key_value]` to integrate your key with the environment
+4. Once you have the key, run `python integrate_key.py [key_value]` to integrate your key with the environment
--- main.py
+++ main.py
@@ -1,3 +1,24 @@
+import google.generativeai as genai
+from key import key
+import os
+genai.configure(api_key=key)
+
+import sys
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def main():
+    # Check if a command-line argument is provided
+    if len(sys.argv) != 2:
+        print("Usage: python main.py <commercial_topic>")
+        sys.exit(1)
+
+    # Get the key_value from the command-line argument
+    commercial_topic = sys.argv[1]
+    
+    script = f"make a commercial speech about {commercial_topic}, intended for about 30-60 sec"
+    response = model.generate_content(script)
+    print(response.text)
 
 # clip creation API
 # video AI API
@@ -5,6 +26,7 @@
 # text AI API
 # speech AI API
 
+'''
 x = "prompt"
 video = video-api-call(x)
 music = audio-api-call("apple commerical")
@@ -12,3 +34,7 @@ text = text-api-call(x)
 speech = speech-api-call(text)
 
 clip = clip-creation-api(video, music, speech)
+'''
+
+if __name__ == "__main__":
+    main()
