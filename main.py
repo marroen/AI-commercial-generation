a26334da6913c6b744e3604b4b48f95512edc99d
Marc Ferriggi <marcferriggi@gmail.com>
2024-10-23 16:25:18 +0200

    added manual method to include video clips

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import base64
 import requests
 import sys
 import re
+import glob
 from random import randrange
 
 # API imports
@@ -65,6 +66,42 @@ def create_video(n):
     combined_audio = CompositeAudioClip([speech.set_start(5), music.set_start(0)])
     video = image_sequence.set_audio(combined_audio)
     video.write_videofile("out/video.mp4", fps=24)
+
+def create_vid2():
+    # Create speech
+    speech = AudioFileClip("out/speech.mp3")
+    speech_length = MP3("out/speech.mp3").info.length
+    # set video duration based on speech length
+    target_duration = speech_length + 7
+
+    videos = glob.glob("out/videos/*.mp4")
+    i = 0
+    duration = 0
+    seq = []
+    while (i < len(videos) and duration < target_duration):
+        clip = VideoFileClip(videos[i])
+        seq.append(clip)
+        duration += clip.duration
+        i += 1
+
+    video_sequence = concatenate_videoclips(seq, method="compose")
+
+    #slow down clips if length of video is too short
+    if video_sequence.duration < speech_length:
+        factor = video_sequence.duration / speech_length
+        video_sequence = video_sequence.fx(vfx.speedx, factor)
+
+    # Load music
+    # prompt: an apple (company) commercial style background song , 30 sec, modernistic, technological
+    # Other prompts for other songs included adding tags like explorative, mysterious, orchestral, and removing the length requirenemt
+    song_id = randrange(3) + 1
+    music = AudioFileClip("out/song_" + str(song_id) + ".mp3").subclip(0, video_sequence.duration).volumex(0.5)
+
+    # Prepare and write videofile
+    combined_audio = CompositeAudioClip([speech.set_start(5), music.set_start(0)])
+    video = video_sequence.set_audio(combined_audio)
+    video.write_videofile("out/video_2.mp4", fps=24)
+
 
 async def create_speech(script):
 
@@ -125,6 +162,7 @@ async def main():
     parser.add_argument('-t', "--topic"   , dest="commercial_topic", required=True,  type=str, help="The commercial topic.")
     parser.add_argument('-g', "--generate", dest="should_generate" , required=False, action='store_true', help="Whether to generate new content or to look for existing content for the video. WARNING: this will use credits from the AI APIs.")
     parser.add_argument('-n',               dest="n",                required=False, type=int, help="The number of images to produce for the video, with each image being on-screen for 15 sec. WARNING: ensure you have enough ElevenLabs credits.")
+    parser.add_argument('-v', "--video"   , dest="video_version"   , required=False, action='store_true', help="This will use the videos already generated in the folder out/videos and the text and speech already in out to generate a video")
     parser.set_defaults(should_generate=False)
     parser.set_defaults(n=5)
 
@@ -136,7 +174,10 @@ async def main():
 
     script = f"make a commercial speech about {commercial_topic}, intended for about {commercial_length} sec, in the style of an Apple (the company) commercial, and include ONLY the actual lines from the narrator, not stuff like 'It was a sunny day', and also do not literally write **Narrator** whenever the narrator speaks, and do not describe the scenery ala 'closeup shot of the banana', I repeat, DO NOT DESCRIBE THE SCENERY NOR WHAT IS HAPPENING IN THE COMMERCIAL, ONLY WRITE EXACTLY WHAT THE NARRATOR SAYS"
 
-    if args.should_generate:
+    if args.video_version:
+        print(f"Generating video only")
+        create_vid2()
+    elif args.should_generate:
         response = gemini_model.generate_content(script)
         processed_text = re.sub("\(.*?\)|\[.*?\]", "", response.text)
         print(processed_text)
@@ -144,7 +185,7 @@ async def main():
         # await create_image(commercial_topic, n)
         # await create_speech(processed_text)
 
-    create_video(n)
+    #create_video(n)
 
 asyncio.run(main())
 
